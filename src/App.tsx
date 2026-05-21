f5f184c339c0cd7eaf1467c55e8f16f77f0e0bbc
Audrey <audreythando@gmail.com>
2026-05-21 14:31:04 +0200

    feat: wrap app in MUI ThemeProvider with dark theme

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,23 @@
+import { ThemeProvider, CssBaseline, Box, Typography } from '@mui/material'
+import { theme } from './theme/theme'
+
 function App() {
-  return <div>Cardify coming soon</div>
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box sx={{
+        minHeight: '100vh',
+        backgroundColor: 'background.default',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+      }}>
+        <Typography variant="h4" color="primary">
+          Cardify is loading... 💳
+        </Typography>
+      </Box>
+    </ThemeProvider>
+  )
 }
 
 export default App
